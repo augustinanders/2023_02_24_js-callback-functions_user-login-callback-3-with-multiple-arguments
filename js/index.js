21d9823de0ed04e3e5bb66a1cbea2e209d2483dc
Augustin Anders <121796762+augustinanders@users.noreply.github.com>
2023-02-25 12:35:28 +0100

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,18 @@
+console.clear();
+
+function handleUserLogin(onSuccess) {
+  onSuccess("Jane Doe", "admin");
+}
+
+// The exercise starts here!
+
+function showWelcomeMessage(userName, userRole) {
+  console.log(`Welcome ${userName}! You are logged in now as ${userRole}.`);
+}
+handleUserLogin(showWelcomeMessage);
+
+//---//
+
+handleUserLogin((userName, userRole) => {
+  console.log(`Welcome ${userName}! You are logged in now as ${userRole}.`);
+});
